9e988efed6bb84f240d533e8e961b294ff26a70e
Davey-M <davey.meuer@gmail.com>
2022-03-03 10:45:40 -0600

    create GifList and GifItem components

---
+++ src/components/GifItem/GifItem.jsx
@@ -0,0 +1,3 @@
+function GifItem() {}
+
+export default GifItem;
---
+++ src/components/GifList/GifList.jsx
@@ -0,0 +1,50 @@
+import GifItem from "../GifItem/GifItem";
+
+const dummyData = [
+  {
+    id: 1,
+    name: "GIF One",
+    url: "https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png",
+  },
+  {
+    id: 2,
+    name: "GIF Two",
+    url: "https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png",
+  },
+  {
+    id: 3,
+    name: "GIF Three",
+    url: "https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png",
+  },
+  {
+    id: 4,
+    name: "GIF Four",
+    url: "https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png",
+  },
+  {
+    id: 5,
+    name: "GIF Five",
+    url: "https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png",
+  },
+  {
+    id: 6,
+    name: "GIF Six",
+    url: "https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png",
+  },
+  {
+    id: 7,
+    name: "GIF Seven",
+    url: "https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png",
+  },
+];
+
+function GifList() {
+  return (
+    <>
+      <h1>This is the GifList component</h1>
+      <ul></ul>;
+    </>
+  );
+}
+
+export default GifList;
