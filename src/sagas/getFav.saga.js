bbb2e84cb1cc17d4782c32d342ddf8b391fce829
Andrew Kapp <dkapp59@gmail.com>
2022-03-04 09:41:40 -0600

    Added getFav saga

---
+++ src/sagas/getFav.saga.js
@@ -0,0 +1,15 @@
+import { put } from 'redux-saga/effects'
+import axios from 'axios'
+
+function* getFav() {
+  try {
+
+    const favResponse = yield axios.get(`/api/favorite/`);
+    yield put({ type: 'GET_FAV_GIFS', payload: favResponse.data })
+  }
+  catch (error) {
+    console.log('Error GETing Fav GIFS', error);
+  }
+}
+
+export default getFav;
