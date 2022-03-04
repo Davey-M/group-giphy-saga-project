f84fd802368eac7d7b8333a60d222ee039539993
Davey-M <davey.meuer@gmail.com>
2022-03-04 10:01:16 -0600

    update reducers and sagas in index

--- src/index.js
+++ src/index.js
@@ -7,16 +7,23 @@ import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
 import { takeEvery, put } from "redux-saga/effects";
 
+// import reducers
 import gifList from "./reducers/gifList.reducer";
+import gifFav from './reducers/getFav.reducer';
+
+// saga imports
 import getGifs from "./sagas/getGifs.saga";
 import postGif from "./sagas/postGif.saga";
 import deleteGif from "./sagas/deleteGifs.saga";
+import getFav from "./reducers/getFav.reducer";
+import updateCategory from "./sagas/updateCategory.saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const storeInstance = createStore(
   combineReducers({
     gifList,
+    gifFav,
   }),
   applyMiddleware(sagaMiddleware, logger)
 );
@@ -25,6 +32,8 @@ function* watcherSaga() {
   yield takeEvery("GET_GIFS", getGifs);
   yield takeEvery("POST_GIF", postGif);
   yield takeEvery("DELETE_FAVE", deleteGif);
+  yield takeEvery('UPDATE_CATEGORY', updateCategory);
+  yield takeEvery('GET_FAVORITES', getFav);
 }
 
 sagaMiddleware.run(watcherSaga);
