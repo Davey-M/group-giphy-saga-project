cf6762794eedf37241a16e2a01e64358656d052c
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-04 10:01:56 -0600

    Imported favGifList in Favorites component

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,11 +1,17 @@
 import GifItem from "../GifItem/GifItem";
+import {useSelector} from 'react-redux';
 
 function Favorites() {
+
+  const favGifList = useSelector((store) => store.getFav);
+
+  console.log(favGifList);
+
   return (
     <>
       <h1>Favorites View</h1>
       <div className='gif-list-container'>
-        {dummyData.map(({ id, name, img_url, img_id, category_id }) => {
+        {favGifList.map(({ id, name, img_url, img_id, category_id }) => {
           return (
             <GifItem
               key={id}
