6a25ba56921130930e49c65d4e5432af9f5b1d6e
Davey-M <davey.meuer@gmail.com>
2022-03-04 09:35:50 -0600

    add favorites view

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,3 +1,5 @@
+import GifItem from "../GifItem/GifItem";
+
 const dummyData = [
   {
     id: 31,
@@ -26,7 +28,25 @@ const dummyData = [
 ];
 
 function Favorites() {
-  return <h1>Favorites View</h1>;
+  return (
+    <>
+      <h1>Favorites View</h1>
+      <div className='gif-list-container'>
+        {dummyData.map(({ id, name, img_url, img_id, category_id }) => {
+          return (
+            <GifItem
+              key={id}
+              name={name}
+              img_id={img_id}
+              img_url={img_url}
+              fav={true}
+              cat={category_id}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Favorites;
