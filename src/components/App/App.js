798be50e4f6d612bf65633ca3b250420c5c5c5ec
Davey-M <davey.meuer@gmail.com>
2022-03-04 09:30:56 -0600

    add dummyData and Favorite Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 
 import GifList from '../GifList/GifList';
 import Search from '../Search/Search';
+import Favorites from '../Favorites/Favorites';
 
 import './App.css'
 
@@ -17,8 +18,9 @@ function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
-      <Search />
-      <GifList />
+      {/* <Search />
+      <GifList /> */}
+      <Favorites />
     </div>
   );
 }
